988a1f367172a0d98f83d2179bff0bfe6cf4a249
jaspreet-singh37349 <jaspreet.singh37349@gmail.com>
2020-05-11 16:19:02 +0530

    navbar updated

--- kiraye-car/src/components/Navbar.jsx
+++ kiraye-car/src/components/Navbar.jsx
@@ -1,78 +1,39 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import {Link} from 'react-router-dom'
+import {HomeOutlined, AccountCircle,PersonAdd} from '@material-ui/icons'
+import HomePage from './HomePage'
 
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
+export default class SimpleTabs extends React.Component{
 
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
+  state={
+    value:null
+  }
 
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
+  handleChange = (event, newValue) => {
+    this.setState({value:newValue})
   };
-}
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
-  },
-}));
-
-export default function SimpleTabs() {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
-        </Tabs>
-      </AppBar>
-      <TabPanel value={value} index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel>
-    </div>
-  );
+  render()
+  {
+    let val = this.state.value!==null?this.state.value:this.props.value
+    
+    return (
+      <div>
+        <AppBar position="static">
+          <Tabs value={val} onChange={this.handleChange} aria-label="simple tabs example">
+            <Tab icon={<HomeOutlined />} label="Home" component={Link} to="/" />
+            <Tab label="Item Two" component={Link} to={"/signup"} />
+            <Tab icon={<AccountCircle/>} style={{marginLeft:"45%"}} label="Login" component={Link} to="/login"  />
+            <Tab icon={<PersonAdd/>} label="SignUp" component={Link} to={"/signup"} />
+          </Tabs>
+        </AppBar>
+        
+      </div>
+    );
+  }
 }
--- kiraye-car/src/components/auth/Login.jsx
+++ kiraye-car/src/components/auth/Login.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
-import Navbar from './Navbar'
 import Button from '@material-ui/core/Button';
 
-export default class HomePage extends Component {
+export default class Login extends Component {
     render() {
         return (
             <div>
-                
                 <Button variant="contained" color="primary">
                     Button
                 </Button>
