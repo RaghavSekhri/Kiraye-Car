5853a0a3031db3caaaba804fff94111739e40565
RaghavSekhri <raghavsekhri2406@gmail.com>
2020-05-11 18:16:44 +0530

    Updated Navbar

--- kiraye-car/src/components/HomePage.jsx
+++ kiraye-car/src/components/HomePage.jsx
@@ -4,7 +4,7 @@ export default class HomePage extends Component {
     render() {
         return (
             <div>
-                Welcome Guys to our HomePage
+                Welcome to HomePage!!!!
             </div>
         )
     }
