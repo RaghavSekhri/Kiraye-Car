17bf535eb5166a30521deb3abd3504cbc5aabe6a
jaspreet-singh37349 <jaspreet.singh37349@gmail.com>
2020-05-13 17:13:57 +0530

    error-validation backend

--- Backend/src/router/user.js
+++ Backend/src/router/user.js
@@ -3,41 +3,48 @@ const User=require('../models/user');
 const auth=require('../middleware/auth');
 const validator=require('validator')
 
+const validate = require('../validation/validd');
 
 const router=new express.Router();
 
 router.post('/user/signup',async (req,res)=>{
-    
-    var a = req.body.name.split(' ');
+    console.log(req.body)
+    const { errors, isValid } = validate(req.body);
 
-    const user=new User(req.body);
-    try{
-        if(!validator.isAlpha(a[0]))
-    {
-        throw new Error('Invalid name');
+    // Check Validation
+    if (!isValid) {
+        return res.status(400).json(errors);
     }
-    if(!validator.isAlpha(a[1]))
-    {
-        throw new Error('Invalid name');
-    }
-        await user.save();
-        const token= await user.generateAuthToken();
-        res.status(201).send({name:user.name,token});
-    }
-    catch(e){
-        return res.status(400).json({ Error : e.toString() });
-    }
+
+    User.findOne({ email: req.body.email }).then(async user => {
+        if (user) {
+          errors.email = 'Email already exists';
+          return res.status(400).json(errors);
+        }
+
+        try{
+            const user=new User(req.body);
+            await user.save();
+            const token= await user.generateAuthToken();
+            res.status(201).send({name:user.name,token});
+        }catch(e){
+            return res.status(400).json({ Error : e.toString() });
+        }
+        
+    })
+
+    
 })
 router.post('/user/login',async (req,res)=>{
     try{
         
         const user=await User.findByCredentials(req.body.email,req.body.password);
         const token=await user.generateAuthToken();
-        res.send({ name:user.name , token});
+        res.send({ name:user.Fname , token});
     }
     catch(e){
-        console.log("error")
-        return res.status(400).json({ Error: e.toString() });
+        console.log(e)
+        return res.status(400).json({ Error: e });
     }
 })
 router.post('/user/logout',auth,async (req,res)=>{
@@ -60,17 +67,25 @@ router.get('/user/me',auth,async (req,res)=>{
     });
 })
 router.patch('/user/update',auth,async(req,res)=>{
-    const allowedUpdates=['name','email','password'];
+    const allowedUpdates=['Fname','Lname','email','password'];
     const updates=Object.keys(req.body);
-    const isValid=updates.every((update)=>allowedUpdates.includes(update))
-    if(!isValid)
+    const valid=updates.every((update)=>allowedUpdates.includes(update))
+    if(!valid)
     {
        return res.status(400).send({error : 'Invalid Updates.'})
     }
+
+    req.body.password2=req.body.password
+    let { errors, isValid } = validate(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     try{
-    updates.forEach((update)=> req.user[update]=req.body[update]);
-    await req.user.save();
-    res.send();
+        updates.forEach((update)=> req.user[update]=req.body[update]);
+        await req.user.save();
+        res.send();
     }
     catch(e)
     {
