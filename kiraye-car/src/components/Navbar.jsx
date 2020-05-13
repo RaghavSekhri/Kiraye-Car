ce7203bfd3e4a3edd5388563940ebd5113f49c0d
jaspreet-singh37349 <jaspreet.singh37349@gmail.com>
2020-05-14 02:14:20 +0530

    navbar updated

--- kiraye-car/src/components/Navbar.jsx
+++ kiraye-car/src/components/Navbar.jsx
@@ -3,14 +3,15 @@ import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import {Link} from 'react-router-dom'
-import {HomeOutlined, AccountCircle,PersonAdd} from '@material-ui/icons'
-import { createMuiTheme } from '@material-ui/core/styles';
+import {HomeOutlined, AccountCircle,PersonAdd, ExitToApp} from '@material-ui/icons'
+import { createMuiTheme, withStyles, ThemeProvider } from '@material-ui/core/styles';
 import '@material/react-tab-indicator/dist/tab-indicator.css';
 
-export default class SimpleTabs extends React.Component{
+class SimpleTabs extends React.Component{
 
   state={
-    value:null
+    value: null,
+    auth: false
   }
 
   handleChange = (event, newValue) => {
@@ -18,26 +19,60 @@ export default class SimpleTabs extends React.Component{
   };
   render()
   {
+    const {classes} = this.props
+    console.log(classes)
+    let val = this.state.value!==null?this.state.value:this.props.value
+
     const theme = createMuiTheme({
       palette: {
         primary: {
           main: '#000'
         }
       },
+      overrides: {
+        MuiTab: {
+          root: {
+            textTransform: 'none',
+            '&:hover': {
+              color: '#fff176',
+              opacity: 1,
+            },
+            '&:focus': {
+              color: 'yellow',
+            },
+            '&$selected': {
+              color: 'yellow',
+            }
+          },
+        },
+        MuiTabs: {
+          indicator: {
+            backgroundColor: 'yellow',
+          },
+        }
+      },
     });
-
-    let val = this.state.value!==null?this.state.value:this.props.value
     
     return (
       <div>
-        <AppBar position="static" style={{backgroundColor:theme.palette.primary.main}}>
-          <Tabs value={val} onChange={this.handleChange} aria-label="simple tabs example">
-            <Tab icon={<HomeOutlined />} label="Home" component={Link} to="/" />
-            <Tab icon={<AccountCircle/>} style={{position: 'absolute', left: '82%'}} label="Login" component={Link} to="/login"  />
-            <Tab icon={<PersonAdd/>} style={{position: 'absolute', left: '89%'}} label="SignUp" component={Link} to={"/signup"} />
-          </Tabs>
-        </AppBar>
+        <ThemeProvider theme={theme}>
+          <AppBar position="static" >
+            <Tabs value={val} onChange={this.handleChange} aria-label="simple tabs example">
+              <Tab classes={{ root: classes.tab }} wrapped icon={<HomeOutlined />} label="Home" component={Link} to="/" />
+              {!this.state.auth && <Tab classes={{ root: classes.tab }} wrapped icon={<AccountCircle/>} style={{marginLeft:"78%"}} label="Login" component={Link} to="/login"  />}
+              {!this.state.auth && <Tab classes={{ root: classes.tab }} wrapped icon={<PersonAdd/>} label="SignUp" component={Link} to={"/signup"} />}
+              {this.state.auth && <Tab classes={{ root: classes.tab }} wrapped icon={<ExitToApp/>} style={{marginLeft:"85%"}} label="LogOut" component={Link} to={"/logout"} />}
+            </Tabs>
+          </AppBar>
+        </ThemeProvider>
       </div>
     );
   }
 }
+
+const styles = {
+  tab: {
+      minWidth: 90
+  }
+};
+export default withStyles(styles)(SimpleTabs);
