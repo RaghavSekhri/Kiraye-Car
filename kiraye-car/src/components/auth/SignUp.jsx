c8e250be9e4b8dbccbb4a945c1a2be95a8130f42
jaspreet-singh37349 <jaspreet.singh37349@gmail.com>
2020-05-16 18:24:20 +0530

    navbar to all components

--- kiraye-car/src/components/auth/SignUp.jsx
+++ kiraye-car/src/components/auth/SignUp.jsx
@@ -13,6 +13,7 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import AuthFeedBack from '../AuthFeedBack'
+import Navbar from '../Navbar'
 import axios from 'axios'
 
 
@@ -81,123 +82,126 @@ class SignUp extends React.Component {
         //console.log(this.state)
         const {errors} = this.state
         return (
-            <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <div style={{marginTop: theme.spacing(8),display: 'flex',flexDirection: 'column',alignItems: 'center'}}>
-                <Avatar style={{margin: theme.spacing(1),backgroundColor: theme.palette.secondary.main}}>
-                <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                Sign up
-                </Typography>
-                <form style={{ width: '100%', marginTop: theme.spacing(3)}} noValidate onSubmit={this.onSubmit}>
-                <Grid container spacing={2}>
-                    <Grid item xs={12} sm={6}>
-                    <TextField
-                        error={errors.Fname!==undefined}
-                        helperText={errors.Fname?errors.Fname:null}
-                        autoComplete="fname"
-                        name="Fname"
-                        variant="outlined"
-                        required
-                        fullWidth
-                        id="firstName"
-                        label="First Name"
-                        autoFocus
-                        onChange={this.handleChange}
-                    />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                    <TextField
-                        error={errors.Lname!==undefined}
-                        helperText={errors.Lname?errors.Lname:null}
-                        variant="outlined"
-                        required
-                        fullWidth
-                        id="lastName"
-                        label="Last Name"
-                        name="Lname"
-                        autoComplete="lname"
-                        onChange={this.handleChange}
-                    />
-                    </Grid>
-                    <Grid item xs={12}>
-                    <TextField
-                        error={errors.email!==undefined}
-                        helperText={errors.email?errors.email:null}
-                        variant="outlined"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        onChange={this.handleChange}
-                    />
-                    </Grid>
-                    <Grid item xs={12}>
-                    <TextField
-                        error={errors.password!==undefined}
-                        helperText={errors.password?errors.password:null}
-                        variant="outlined"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                        onChange={this.handleChange}
-                    />
-                    </Grid>
-                    <Grid item xs={12}>
-                    <TextField
-                        error={errors.password2!==undefined}
-                        helperText={errors.password2?errors.password2:null}
-                        variant="outlined"
-                        required
-                        fullWidth
-                        name="password2"
-                        label="Confirm Password"
-                        type="password"
-                        id="password2"
-                        autoComplete="current-password"
-                        onChange={this.handleChange}
-                    />
-                    </Grid>
-                    <Grid item xs={12}>
-                    <FormControlLabel
-                        control={<Checkbox value="allowExtraEmails" color="primary" />}
-                        label="I want to receive inspiration, marketing promotions and updates via email."
-                    />
-                    </Grid>
-                </Grid>
-                <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                    style={{margin: theme.spacing(3, 0, 2)}}
-                >
-                    Sign Up
-                </Button>
-                <Grid container justify="flex-end">
-                    <Grid item>
-                    <Link to="/login" variant="body2">
-                        Already have an account? Sign in
-                    </Link>
-                    </Grid>
-                </Grid>
-                </form>
-                <AuthFeedBack 
-                    txt="Successfully Logged In"
-                    open={this.state.open} 
-                    autoHideDuration={6000}
-                    onClose={this.handleClose}
-                    severity="success" 
-                />
+            <div>
+                <Navbar value={2}/>
+                <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                    <div style={{marginTop: theme.spacing(8),display: 'flex',flexDirection: 'column',alignItems: 'center'}}>
+                        <Avatar style={{margin: theme.spacing(1),backgroundColor: theme.palette.secondary.main}}>
+                        <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                        Sign up
+                        </Typography>
+                        <form style={{ width: '100%', marginTop: theme.spacing(3)}} noValidate onSubmit={this.onSubmit}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={6}>
+                            <TextField
+                                error={errors.Fname!==undefined}
+                                helperText={errors.Fname?errors.Fname:null}
+                                autoComplete="fname"
+                                name="Fname"
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="firstName"
+                                label="First Name"
+                                autoFocus
+                                onChange={this.handleChange}
+                            />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                            <TextField
+                                error={errors.Lname!==undefined}
+                                helperText={errors.Lname?errors.Lname:null}
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="lastName"
+                                label="Last Name"
+                                name="Lname"
+                                autoComplete="lname"
+                                onChange={this.handleChange}
+                            />
+                            </Grid>
+                            <Grid item xs={12}>
+                            <TextField
+                                error={errors.email!==undefined}
+                                helperText={errors.email?errors.email:null}
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                onChange={this.handleChange}
+                            />
+                            </Grid>
+                            <Grid item xs={12}>
+                            <TextField
+                                error={errors.password!==undefined}
+                                helperText={errors.password?errors.password:null}
+                                variant="outlined"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                                onChange={this.handleChange}
+                            />
+                            </Grid>
+                            <Grid item xs={12}>
+                            <TextField
+                                error={errors.password2!==undefined}
+                                helperText={errors.password2?errors.password2:null}
+                                variant="outlined"
+                                required
+                                fullWidth
+                                name="password2"
+                                label="Confirm Password"
+                                type="password"
+                                id="password2"
+                                autoComplete="current-password"
+                                onChange={this.handleChange}
+                            />
+                            </Grid>
+                            <Grid item xs={12}>
+                            <FormControlLabel
+                                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                                label="I want to receive inspiration, marketing promotions and updates via email."
+                            />
+                            </Grid>
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            style={{margin: theme.spacing(3, 0, 2)}}
+                        >
+                            Sign Up
+                        </Button>
+                        <Grid container justify="flex-end">
+                            <Grid item>
+                            <Link to="/login" variant="body2">
+                                Already have an account? Sign in
+                            </Link>
+                            </Grid>
+                        </Grid>
+                        </form>
+                        <AuthFeedBack 
+                            txt="Successfully Logged In"
+                            open={this.state.open} 
+                            autoHideDuration={6000}
+                            onClose={this.handleClose}
+                            severity="success" 
+                        />
+                    </div>
+                </Container>
             </div>
-            </Container>
         );
     }
 }
