68c2d852820fe693fdafbc6454d0e52f90c6b397
jaspreet-singh37349 <jaspreet.singh37349@gmail.com>
2020-06-06 02:14:44 +0530

    frontend returncar updated

--- kiraye-car/src/components/ReturnCarCard.jsx
+++ kiraye-car/src/components/ReturnCarCard.jsx
@@ -1,11 +1,29 @@
 import React, {Component} from 'react';
 import Card from '@material-ui/core/Card';
 import Button from '@material-ui/core/Button';
+import AuthFeedBack from './AuthFeedBack'
+import Loader from './Loader'
 import axios from 'axios'
 
 export default class ReturnCarCard extends Component{
 
+    state={
+        cars:this.props.cars,
+        load:false,
+        open:false
+    }
+
+    handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        this.setState({open:false})
+    }
+
     handleReturn = (carId) => {
+
+        this.setState({load:true})
 
         let obj={
             carId:carId
@@ -13,19 +31,18 @@ export default class ReturnCarCard extends Component{
 
         axios.patch("https://kiraye-car.herokuapp.com/returncar",obj).then(res=>{
             console.log(res)
+            this.setState({cars:this.state.cars.filter(car=>car._id!==carId),load:false,open:true})
         })
     }
 
     render()
     {
-        console.log(this.props.cars)
-        
 
         return (
             <div>
                 <h1>{this.props.type} Cars</h1>
                 <div style={{display:"flex",flexWrap:"wrap"}}>
-                    {this.props.cars.map(car=>{
+                    {this.state.cars.map(car=>{
                         return(
                             <Card style={{margin:"30px"}}>
                                 <img style={{width:"300px",height:"300px"}} src={car.media.images[0]} alt="error"/>
@@ -35,6 +52,14 @@ export default class ReturnCarCard extends Component{
                         )
                     })}
                 </div>
+                <AuthFeedBack 
+                    txt="Returned Car!!"
+                    open={this.state.open} 
+                    autoHideDuration={1000}
+                    onClose={this.handleClose}
+                    severity="success" 
+                />
+                <Loader open={this.state.load} />
             </div>
         );
     }
